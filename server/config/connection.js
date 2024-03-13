ee131088cd389fa4ae5018e414c83754a1f30f0a
Jarede712 <jaredeic@yahoo.com>
2024-03-12 22:26:41 -0700

    updated connection to try and fix render deploy failing

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI ||
-    'mongodb+srv://jaredeichhorst:Fa0TcSLWzBcoke3p@cluster0.rh8orni.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks'
 );
 
 module.exports = mongoose.connection;
