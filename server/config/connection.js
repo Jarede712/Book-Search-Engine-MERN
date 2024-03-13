545db94bee6ed747e77fb0025d0da25f70b2afb1
Jarede712 <jaredeic@yahoo.com>
2024-03-12 22:22:12 -0700

    added code to try and fix render deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Database connected successfully'))
-  .catch((err) => console.error('Database connection error', err));
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    'mongodb+srv://jaredeichhorst:Fa0TcSLWzBcoke3p@cluster0.rh8orni.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+);
 
 module.exports = mongoose.connection;
