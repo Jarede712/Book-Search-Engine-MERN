06287248025b685c588efd78555554e53b27058f
Jarede712 <jaredeic@yahoo.com>
2024-03-12 21:25:07 -0700

    added some error catching to figure out why render wont connect to mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks'
-);
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Database connected successfully'))
+  .catch((err) => console.error('Database connection error', err));
 
 module.exports = mongoose.connection;
